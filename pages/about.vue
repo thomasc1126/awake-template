d5b0f0fa1b5522afcf90d7551ab06950cba17da4
thomasc1126 <30421570+thomasc1126@users.noreply.github.com>
2021-03-08 17:16:06 -0500

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -105,3 +105,11 @@ export default {
   components: { QuoteCard }
 }
 </script>
+
+
+<nft-card
+  tokenAddress="0x5caebd3b32e210e85ce3e9d51638b9c445481567"
+  tokenId="2242579050293992223"
+  network="mainnet"
+  >
+</nft-card>
